82f5b47adcc7acc63363bd50ae4b4545370cb22a
Mapeadoh <philcotablet681@gmail.com>
2021-07-06 15:37:44 -0300

    idk

---
+++ src/main/java/me/travis/mapeadoh/clientstuff/wp3/Globals.java
@@ -0,0 +1,16 @@
+package me.travis.mapeadoh.clientstuff.wp3;
+
+import net.minecraft.client.Minecraft;
+
+import java.util.Random;
+
+public interface Globals {
+    Minecraft mc = Minecraft.getMinecraft();
+    Random random = new Random();
+    char SECTIONSIGN = '\u00A7';
+
+    default public boolean nullCheck(){
+        return mc.player == null || mc.world == null;
+    }
+
+}
