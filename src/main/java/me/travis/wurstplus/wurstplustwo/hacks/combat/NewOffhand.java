32f5330cc9959dff883375126890265ff66c726d
Mapeadoh <philcotablet681@gmail.com>
2021-07-16 16:35:50 -0300

    this is like a checkpoint, i dont want to lost the code

--- src/main/java/me/travis/wurstplus/wurstplustwo/hacks/combat/NewOffhand.java
+++ src/main/java/me/travis/wurstplus/wurstplustwo/hacks/combat/NewOffhand.java
@@ -1,43 +1,61 @@
 package me.travis.wurstplus.wurstplustwo.hacks.combat;
 
+import com.mojang.realmsclient.gui.ChatFormatting;
 import me.travis.wurstplus.AnasheClient;
 import me.travis.wurstplus.wurstplustwo.guiscreen.settings.WurstplusSetting;
 import me.travis.wurstplus.wurstplustwo.hacks.WurstplusCategory;
 import me.travis.wurstplus.wurstplustwo.hacks.WurstplusHack;
+import me.travis.wurstplus.wurstplustwo.util.WurstplusMessageUtil;
 import me.travis.wurstplus.wurstplustwo.util.WurstplusPlayerUtil;
 import net.minecraft.client.gui.inventory.GuiInventory;
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.inventory.ClickType;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.BlockPos;
 
 
 public class NewOffhand extends WurstplusHack {
+    public static NewOffhand INSTANCE;
+
     public NewOffhand() {
         super(WurstplusCategory.WURSTPLUS_COMBAT);
 
-        this.name        = "NewOffhandNotWorking";
-        this.tag         = "NewOffhandNotWorking";
-        this.description = "offhand wip, not active this";
+        this.name = "NewOffhand";
+        this.tag = "NewOffhand";
+        this.description = "offhand test";
+        INSTANCE = this;
     }
-    WurstplusSetting info = create("info", "NotActiveThis", "WIPNoActive");
+
+    WurstplusSetting info = create("info", "Info", "InTest");
     public WurstplusSetting switch_mode = create("Offhand", "OffhandOffhand", "Totem", combobox("Totem", "Crystal", "Gapple", "None"));
     WurstplusSetting totem_switch = create("Totem HP", "OffhandTotemHP", 16, 0, 36);
-
+    //another settings
     WurstplusSetting gapple_in_hole = create("Gapple In Hole", "OffhandGapple", false);
     WurstplusSetting gapple_hole_hp = create("Gapple Hole HP", "OffhandGappleHP", 8, 0, 36);
-
+    WurstplusSetting totemonfall = create("TotemOnFall", "TotemOnFall", false);
+    WurstplusSetting falldistance = create("FallDistance", "Distance", 24, 3, 100);
     WurstplusSetting sword_gapple = create("Gapple in Sword", "SwordGapple", false);
     WurstplusSetting sword_gapple_hp = create("Sword Gapple HP", "SwordGappleHP", 20, 0, 36);
+    //WurstplusSetting totemonelytra = create("ElytraTotem", "ElytraTotem", true);
+    WurstplusSetting debug = create("Debug", "Debug", false);
 
     WurstplusSetting delay = create("Delay", "OffhandDelay", false);
 
     private boolean switching = false;
     private int last_slot;
 
+    private ChatFormatting r = ChatFormatting.RESET;
+    private ChatFormatting da = ChatFormatting.DARK_AQUA;
+    private ChatFormatting g = ChatFormatting.GRAY;
+    private ChatFormatting go = ChatFormatting.GOLD;
+
     @Override
     public void update() {
+        if (mc.player.isCreative()) {
+            this.toggle();
+        }
 
         if (mc.currentScreen == null || mc.currentScreen instanceof GuiInventory) {
 
@@ -50,17 +68,22 @@ public class NewOffhand extends WurstplusHack {
 
             if (hp > totem_switch.get_value(1)) {
                 if (switch_mode.in("Crystal") && AnasheClient.get_hack_manager().get_module_with_tag("NewAutoCrystal").is_active()) {
-                    swap_items(get_item_slot(Items.END_CRYSTAL),0);
+                    swap_items(get_item_slot(Items.END_CRYSTAL), 0);
                     return;
                 }
                 if (gapple_in_hole.get_value(true) && hp > gapple_hole_hp.get_value(1) && is_in_hole()) {
                     swap_items(get_item_slot(Items.GOLDEN_APPLE), delay.get_value(true) ? 1 : 0);
                     return;
                 }
-                if (sword_gapple.get_value(true) && hp > sword_gapple_hp.get_value(1) && mc.player.getHeldItemMainhand().getItem() == Items.DIAMOND_SWORD){
+                if (sword_gapple.get_value(true) && hp > sword_gapple_hp.get_value(1) && mc.player.getHeldItemMainhand().getItem() == Items.DIAMOND_SWORD) {
                     swap_items(get_item_slot(Items.GOLDEN_APPLE), delay.get_value(true) ? 1 : 0);
                     return;
                 }
+                if (totemonfall.get_value(true) && mc.player.fallDistance >= falldistance.get_value(1)) {
+                    swap_items(get_item_slot(Items.TOTEM_OF_UNDYING), delay.get_value(true) ? 1 : 0);
+                    return;
+                }
+                //mode
                 if (switch_mode.in("Totem")) {
                     swap_items(get_item_slot(Items.TOTEM_OF_UNDYING), delay.get_value(true) ? 1 : 0);
                     return;
@@ -70,7 +93,7 @@ public class NewOffhand extends WurstplusHack {
                     return;
                 }
                 if (switch_mode.in("Crystal") && !AnasheClient.get_hack_manager().get_module_with_tag("NewAutoCrystal").is_active()) {
-                    swap_items(get_item_slot(Items.TOTEM_OF_UNDYING),0);
+                    swap_items(get_item_slot(Items.TOTEM_OF_UNDYING), 0);
                     return;
                 }
             } else {
@@ -85,7 +108,7 @@ public class NewOffhand extends WurstplusHack {
         }
 
     }
-
+    String iteminoffhand = "";
     public void swap_items(int slot, int step) {
         if (slot == -1) return;
         if (step == 0) {
@@ -102,6 +125,20 @@ public class NewOffhand extends WurstplusHack {
             mc.playerController.windowClick(0, 45, 0, ClickType.PICKUP, mc.player);
             mc.playerController.windowClick(0, slot, 0, ClickType.PICKUP, mc.player);
             switching = false;
+            Item offhanditem = mc.player.getHeldItemOffhand().getItem();
+            if (Items.END_CRYSTAL.equals(offhanditem)) {
+                iteminoffhand = "Ender Crystal";
+                return;
+            } else if (Items.GOLDEN_APPLE.equals(offhanditem)) {
+                iteminoffhand = "Gapple";
+                return;
+            } else if (Items.TOTEM_OF_UNDYING.equals(offhanditem)) {
+                iteminoffhand = "Totem";
+                return;
+            }
+            if(debug.get_value(true)){
+                WurstplusMessageUtil.send_client_message(g + "[" + r + da + "OffHandModule" + r + g + "]" + r + " Now switching to: " + go + iteminoffhand);
+            }
         }
 
         mc.playerController.updateController();
@@ -120,9 +157,9 @@ public class NewOffhand extends WurstplusHack {
 
     private int get_item_slot(Item input) {
         if (input == mc.player.getHeldItemOffhand().getItem()) return -1;
-        for(int i = 36; i >= 0; i--) {
+        for (int i = 36; i >= 0; i--) {
             final Item item = mc.player.inventory.getStackInSlot(i).getItem();
-            if(item == input) {
+            if (item == input) {
                 if (i < 9) {
                     if (input == Items.GOLDEN_APPLE) {
                         return -1;
@@ -134,5 +171,4 @@ public class NewOffhand extends WurstplusHack {
         }
         return -1;
     }
-
 }
